b01f0a8711331d7ea1afba0e3698ba205ae69171
Itzel conde <chelconde46@gmail.com>
2025-11-24 15:49:42 -0600

    Configuracion inicial del proyecto

---
+++ djangostore/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+
+from djangostore import settings 
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('home.urls')),      
+    path('products/', include('products.urls')),  
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
